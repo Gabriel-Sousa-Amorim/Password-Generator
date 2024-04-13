74a9010fa446d16ab77b64afac62769c66825d0f
K <97068125+Gabriel-Sousa-Amorim@users.noreply.github.com>
2024-04-13 11:24:38 -0300

    Create Gerador.cpp

---
+++ Gerador.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <random>
+#include <time.h>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+    srand((unsigned) time(NULL));
+    string letters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzÇç", numbers = "0123456789",  symbols = "!@#$%&*()-_+=^{}[];:?", password = "";
+    int i = 0, size = 15;
+    while (i < size) {
+        i++;
+        switch (1 + rand() % 3)  {
+        case (1):
+            password += (letters[0 + rand() % letters.length()]);
+            break;
+        case (2):
+            password += (numbers[0 + rand() % numbers.length()]);
+            break;
+        case (3):
+            password += (symbols[0 + rand() % symbols.length()]);
+            break;
+        };
+    };
+    cout << password << endl;
+};
